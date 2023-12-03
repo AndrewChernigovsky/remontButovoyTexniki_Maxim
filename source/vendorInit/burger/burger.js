6b27cb73c81a9533b7a73fee3867412398fc6768
Andrew Chernigovsky <chernigovsky108@gmail.com>
2023-12-03 19:29:13 +0300

    not goof

--- source/vendorInit/burger/burger.js
+++ source/vendorInit/burger/burger.js
@@ -1,21 +1,28 @@
 let body = document.querySelector('body');
-let button = document.getElementById('btn-menu');
+let header = document.querySelector('header');
+let button = body.querySelector('#btn-menu');
 let burgerNav = document.querySelector('.navigation');
-let navLinks = document.querySelectorAll('.navigation__list-link');
+let burgerNaWrapper = document.querySelector('.navigation__wrapper');
+let navLinks = body.querySelectorAll('.navigation__list-link');
 let classActive = 'js-active-open';
 let classNoActive = 'no-js';
+let classVisuallyHidden = 'visually-hidden';
 
 export function burger() {
+	if (header) {
+		header.classList.remove(classNoActive);
+	}
 	if (button) {
 		button.addEventListener("click", function () {
 			burgerNav.classList.toggle(classActive);
-			burgerNav.classList.toggle(classNoActive);
+			// burgerNav.classList.toggle(classNoActive);
+			// burgerNaWrapper.classList.toggle(classVisuallyHidden);
 		});
 
 		navLinks.forEach((el) => {
 			el.addEventListener("click", function () {
 				burgerNav.classList.remove(classActive);
-				burgerNav.classList.add(classNoActive);
+				// burgerNav.classList.add(classNoActive);
 			});
 		});
 	}
